785c6f56e5f9317c7547605cf07d45d2967a491d
Evgeny Panshin <117942554+GilgameshEU@users.noreply.github.com>
2023-04-16 16:54:57 +0400

    table list

--- pdfToSql.py
+++ pdfToSql.py
@@ -14,14 +14,23 @@ db_config = {
 engine = create_engine(f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}")
 
 # Чтение таблицы из PDF и преобразование в DataFrame
-df_list = tabula.read_pdf('data.pdf', pages='all',stream=True)
+df_list = tabula.read_pdf('test_pdf/data.pdf', pages='all',stream=True)
 print(f"Успешно извлечено таблиц-{len(df_list)}")
 
 # Запись DataFrame в базу данных
-df_list[0].rename(columns={'Unnamed: 0': 'name'}).to_sql(name='mytable', con=engine, if_exists='append', index=False)
+for i, df in enumerate(df_list):
+    table_name = f"mytable_{i+1}"
+    df.rename(columns={'Unnamed: 0': 'name'}).to_sql(name=table_name, con=engine, if_exists='append', index=False)
 
-# Запрос данных из таблицы
-df = pd.read_sql('SELECT * FROM mytable', con=engine)
+# Запрос данных из всех таблиц
+all_tables = []
+for i in range(len(df_list)):
+    table_name = f"mytable_{i+1}"
+    table_df = pd.read_sql(f'SELECT * FROM {table_name}', con=engine)
+    all_tables.append(table_df)
+
+# Объединение всех таблиц в один DataFrame
+df = pd.concat(all_tables, ignore_index=True)
 
 # Вывод данных в консоль
 print(df)
